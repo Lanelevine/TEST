ee231d5183f5854fd1f06ae412b480e6a6ce3a4f
손준기 <zlfooltime@gmail.com>
2019-05-22 12:01:58 +0900

    test

---
+++ Hitest/Hitest/Login.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Hitest
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.BackColor = System.Drawing.Color.SkyBlue;
+            this.Login.Location = new System.Drawing.Point(12, 249);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 42);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "button1";
+            this.Login.UseVisualStyleBackColor = false;
+            // 
+            // Cancel
+            // 
+            this.Cancel.BackColor = System.Drawing.Color.SkyBlue;
+            this.Cancel.Location = new System.Drawing.Point(128, 249);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 42);
+            this.Cancel.TabIndex = 1;
+            this.Cancel.Text = "button2";
+            this.Cancel.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 323);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Button Cancel;
+    }
+}
+
